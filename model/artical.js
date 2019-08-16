f67c5b3c45098e9646394777774da19603d28067
Ericgoodboy <776968336@qq.com>
2019-08-16 17:41:40 +0800

    把文章从文件中读出来

--- model/artical.js
+++ model/artical.js
@@ -58,24 +58,50 @@ const updateOrInsert=function(data) {
 const update=function(data) {
 }
 const insert=function(data) {
-    let sql = "INSERT INTO atical (aid,title,author,time,subscrib) VALUES (?,?,?,now(),?);"
+    let sql = "INSERT INTO atical (aid,title,author,time,subscrib,type) VALUES (?,?,?,now(),?,?);"
     const dom = new JSDOM(data.editorContent);
-    desc = dom.window.document.firstElementChild.innerHTML
+    desc = dom.window.document.getElementsByTagName("p")[0]
     let path = baseDir+data.id+".dat"
     fs.writeFile(path,data.editorContent,()=>{})
-    arr = [data.id,data.title,"mayeye",desc]
+    arr = [data.id,data.title,"mayeye","描述自己写",data.d_type]
     let connection = getConnection()
-    
     connection.query(sql,arr,function(err,res){
         if(err){
             console.log(err)
         }else(console.log(res))
         connection.commit()
-        console.log("commit")
+        connection.end()
+    })
+}
+const getfulllArtical=function(id,callback){
+    let sql = 'select * from atical where aid = ?'
+    let connection = getConnection()
+    connection.query(sql, [id],function (err, res, fields) {
+        if (err) { callback(err, null) }
+        else{
+            if(res.length>0){
+            let path = baseDir+res[0].aid+".dat"
+            fs.readFile(path,function(err,resf){
+                    if(err){
+                    }else{
+                        // console.dir(String(resf))
+                        let data = {
+                            id:res[0].aid,
+                            title:res[0].title,
+                            body:String(resf),
+                            user:res[0].author,
+                            saw:res[0].saw,
+                            type:res[0].type
+                        }
+                        callback(err,data)
+                    }
+            })
+            }
+        }
         connection.end()
     })
 }
 
 
-artical = {getAllArticals,testhas,updateOrInsert,geTopArticals}
+artical = {getAllArticals,testhas,updateOrInsert,geTopArticals,getfulllArtical}
 module.exports = artical
