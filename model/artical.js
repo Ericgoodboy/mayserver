7ddd349d5eb12271a86ecc44bd9c7dca5072dd64
Ericgoodboy <776968336@qq.com>
2019-08-19 17:29:21 +0800

    0.0.2

--- model/artical.js
+++ model/artical.js
@@ -25,6 +25,16 @@ const geTopArticals=function(callback) {
         connection.end()
     })
 }
+const geArticalsBytype=function(data,callback) {
+    let sql = 'select * from atical where type = ?'
+    // console.log(connection.state)
+    let connection = getConnection()
+    connection.query(sql,[data.type], function (err, res, fields) {
+        if (err) { callback(err, null) }
+        callback(null, res)
+        connection.end()
+    })
+}
 const testhas = function(aid,callback){
     let sql = 'select count(*) as c from atical where aid = ?'
     // console.log(connection.state)
@@ -56,14 +66,25 @@ const updateOrInsert=function(data) {
     })
 }
 const update=function(data) {
+    let path = baseDir+data.id+".dat"
+    let path2 = baseDir+data.id+".md"
+    fs.writeFile(path,data.editorContent,()=>{
+        console.log("html updated")
+    })
+    fs.writeFile(path2,data.value,()=>{
+        console.log("markdown updated")
+    })
 }
 const insert=function(data) {
     let sql = "INSERT INTO atical (aid,title,author,time,subscrib,type) VALUES (?,?,?,now(),?,?);"
     const dom = new JSDOM(data.editorContent);
     desc = dom.window.document.getElementsByTagName("p")[0]
     let path = baseDir+data.id+".dat"
+    let path2 = baseDir+data.id+".md"
     fs.writeFile(path,data.editorContent,()=>{})
-    arr = [data.id,data.title,"mayeye","描述自己写",data.d_type]
+    fs.writeFile(path2,data.value,()=>{})
+
+    arr = [data.id,data.title,"mayeye","预览正在开发...",data.d_type]
     let connection = getConnection()
     connection.query(sql,arr,function(err,res){
         if(err){
@@ -91,8 +112,48 @@ const getfulllArtical=function(id,callback){
                             body:String(resf),
                             user:res[0].author,
                             saw:res[0].saw,
-                            type:res[0].type
+                            type:res[0].type,
+                            value:"# hello mark down"
                         }
+                        
+                        callback(err,data)
+                    }
+            })
+            }
+        }
+        connection.end()
+    })
+}
+const getallinfo=function(id,callback){
+    let sql = 'select * from atical where aid = ?'
+    let connection = getConnection()
+    connection.query(sql, [id],function (err, res, fields) {
+        if (err) { callback(err, null) }
+        else{
+            if(res.length>0){
+            let path = baseDir+res[0].aid+".dat"
+            fs.readFile(path,function(err,resf){
+                    if(err){
+                    }else{
+                        // console.dir(String(resf))
+                        md = "# hello md"
+                        try {
+                            md = String(fs.readFileSync(baseDir+res[0].aid+".md"))
+                        } catch (error) {
+                            
+                        }
+                        console.log(md)
+                        let data = {
+                            id:res[0].aid,
+                            title:res[0].title,
+                            body:String(resf),
+                            user:res[0].author,
+                            saw:res[0].saw,
+                            value:md,
+                            type:res[0].type,
+                            
+                        }
+                        
                         callback(err,data)
                     }
             })
@@ -103,5 +164,6 @@ const getfulllArtical=function(id,callback){
 }
 
 
-artical = {getAllArticals,testhas,updateOrInsert,geTopArticals,getfulllArtical}
+artical = {getAllArticals,testhas,updateOrInsert,geTopArticals,
+    getallinfo,getfulllArtical,geArticalsBytype}
 module.exports = artical
