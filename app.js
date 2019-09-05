ae2ce8d8b0382508f47008fd3e48a4b63614969a
Ericgoodboy <776938336@qq.com>
2019-09-05 10:54:11 +0800

    changeupload

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
-
+app.use("/img",express.static(path.join(__dirname, 'img')));
 app.use('/', apiRouter);
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use("/file",fileRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  res.redirect("/");
+  
 });
 
 // error handler
--- routes/uploadFile.js
+++ routes/uploadFile.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
-router.post("/upload",function(req,res,next){
-    console.log(req.body)
-    var imgurl = "/img/"+(new Date).getTime()
-    res.send("")
+var multer = require("multer");
+
+var storage = multer.diskStorage({
+
+    //将上传的文件存储在指定的位置（不存在的话需要手动创建）
+    destination: function (req, file, cb) {
+        console.log("------------")
+        cb(null, './img')
+    },
+    //将上传的文件做名称的更改
+    filename: function (req, file, cb) {
+        let arr= file.originalname.split(".")
+        let prefix = arr[arr.length-1]
+        let fullname = Date.now() + "-" + "markdown."+prefix
+        cb(null, Date.now() + "-" + file.originalname.split("."))
+    }
+})
+//创建multer对象
+var upload = multer({ storage: storage })
+
+//指定当前字段可以携带多个文件
+// var cpUpload = upload.fields([{ name: 'image', maxCount: 1 }])
+
+router.post("/upload", upload.single('image'),function (req, res, next) {
+    console.log(req.file)
+    res.send("/img/"+req.file.filename)
 })
 module.exports = router;
