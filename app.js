4111d4c7f27661069993885a1f2c7f9c3acb1841
Ericgoodboy <776968336@qq.com>
2020-03-19 10:09:00 +0800

    ii

--- app.js
+++ app.js
@@ -23,14 +23,12 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use("/img",express.static(path.join(__dirname, 'img')));
-app.use('/', apiRouter);
+//app.use('/', apiRouter);
 // app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/file",fileRouter)
+//app.use('/users', usersRouter);
+//app.use("/file",fileRouter)
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  
-});
+
 
 // error handler
 app.use(function(err, req, res, next) {
